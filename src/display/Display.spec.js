cc0999838dd329cfdf09921c61a55a9654399195
Anthony Hill <hillan1152@gmail.com>
2019-10-14 17:57:49 -0400

    took 4 hours, but now I can finally work on the project

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,14 @@
 // Test away!
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Display from './Display';
+
+test('It renders correctly', () => {
+    render(<Display/>)
+});
+
+test('Door is Closed', () => {
+    
+
+})
