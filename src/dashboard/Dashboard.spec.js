5f9ee00d8313f910d99a8a530afabd415731eb3d
Anthony Hill <hillan1152@gmail.com>
2019-10-14 18:29:46 -0400

    dashboard working?

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,11 @@
 // Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Dashboard from './Dashboard';
+import Display from '../display/Display';
+import Controls from '../controls/Controls';
+
+test('Both Render Correctly', () => {
+    render(<Dashboard/>).findAllByDisplayValue(<Display/>, <Controls/>)
+})
