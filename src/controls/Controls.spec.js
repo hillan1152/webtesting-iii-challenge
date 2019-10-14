82bd054693542be896e5894d8c93ff7f9c6dade9
Anthony Hill <hillan1152@gmail.com>
2019-10-14 19:06:19 -0400

    working on controls

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,17 @@
 // Test away!
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Controls from './Controls';
+
+test('Controls Render Correctly', () => {
+    render(<Controls/>)
+});
+
+test('Toggle Lock Gate', ({ closed, locked, toggleLocked}) => {
+    const { getTextBy } = render(<Controls disabled={!closed} onClick={toggleLocked}/>)
+    const doorStatus = getTextBy(/locked/i)
+    const openStatus = getTextBy(/closed/i)
+    expect(doorStatus.classList.contains('Lock Gate')).toBe(true);
+    expect(openStatus.classList.contains('Close Gate')).toBe(true);
+})
