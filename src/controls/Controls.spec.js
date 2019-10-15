c98762ed17f8edc2132251af692ac842d1a19241
Anthony Hill <hillan1152@gmail.com>
2019-10-15 11:04:42 -0400

    completed project

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,6 +1,7 @@
 // Test away!
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import Dashboard from '../dashboard/Dashboard'
 
 import Controls from './Controls';
 
@@ -8,10 +9,14 @@ test('Controls Render Correctly', () => {
     render(<Controls/>)
 });
 
-test('Toggle Lock Gate', ({ closed, locked, toggleLocked}) => {
-    const { getTextBy } = render(<Controls disabled={!closed} onClick={toggleLocked}/>)
-    const doorStatus = getTextBy(/locked/i)
-    const openStatus = getTextBy(/closed/i)
-    expect(doorStatus.classList.contains('Lock Gate')).toBe(true);
-    expect(openStatus.classList.contains('Close Gate')).toBe(true);
+test('Toggle Buttons', () => {
+   const { getByText } = render(<Dashboard/>);
+   getByText(/lock gate/i);
+   fireEvent.click(getByText(/close gate/i));
+   getByText(/open gate/i);
+   expect(getByText(/lock gate/i).disabled).toBe(false);
+   fireEvent.click(getByText(/lock gate/i));
+   getByText(/unlock gate/i);
+   expect(getByText(/open gate/i).disabled).toBe(true);
+
 })
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,11 +1,22 @@
 // Test away
 import React from 'react';
 import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer'
+
+
 
 import Dashboard from './Dashboard';
-import Display from '../display/Display';
-import Controls from '../controls/Controls';
 
-test('Both Render Correctly', () => {
-    render(<Dashboard/>).findAllByDisplayValue(<Display/>, <Controls/>)
+
+describe('<Dashboard/>', () => {
+    it('Should match snapshot', () => {
+        const dash = renderer.create(<Dashboard/>).toJSON();
+        expect(dash).toMatchSnapshot();
+    })
+})
+
+test('They are working', () => {
+    const { getByText } = render(<Dashboard/>);
+    getByText(/open/i);
+    getByText(/close gate/i)
 })
